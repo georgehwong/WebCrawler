8919e78827f8a755a7bc084e5f01075f973f9a94
georgehwong <huangshangru@126.com>
2016-07-21 00:43:41 -0700

    Add WebCrawler changes

--- app/src/main/java/com/project/shangruhuang/webcrawler/WebCrawler.java
+++ app/src/main/java/com/project/shangruhuang/webcrawler/WebCrawler.java
@@ -1,5 +1,10 @@
 package com.project.shangruhuang.webcrawler;
 
+import android.content.Context;
+
+import java.util.HashSet;
+import java.util.concurrent.BlockingQueue;
+
 public class WebCrawler {
     /**
      * Interface for crawling callback
@@ -12,5 +17,20 @@ public class WebCrawler {
         void onCrawlingCompleted();
     }
 
+    private Context mContext;
+    // SQLiteOpenHelper object for handling crawling database
+    private CrawlerDB mCrawlerDB;
+    // Set containing already visited URls
+    private HashSet<String> crawledURL;
+    // Queue for unvisited URL
+    BlockingQueue<String> uncrawledURL;
+    // For parallel crawling execution using ThreadPoolExecuter
+    RunnableManager mManager;
+    // Callback interface object to notify UI
+    CrawlingCallback callback;
+    // For sync of crawled and yet to crawl url lists
+    Object lock;
 
+    private class RunnableManager {
+    }
 }
